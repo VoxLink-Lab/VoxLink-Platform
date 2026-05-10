695dae38e236eb57f5d7f2da5dfcbab081481929
tinsae zegeye <tinsaezegeye@gmail.com>
2026-05-10 13:18:14 +0300

    Add database connection class

--- src/voxlink/server/src/main/database/DBConnection.java
+++ src/voxlink/server/src/main/database/DBConnection.java
@@ -1,5 +1,24 @@
 package voxlink.server.src.main.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    // TODO
+    private static final String url = "jdbc:mysql://localhost:3306/voxlink_db";
+    private static final String password = "";
+    private static final String user = "root";
+
+    public static Connection connect() {
+        Connection connection;
+        try{
+            connection = DriverManager.getConnection(DBConnection.url, DBConnection.user, DBConnection.password);
+
+        } catch(SQLException e) {
+            System.out.println("Unable to connect to the database!!");
+            throw new RuntimeException(e.getMessage());
+        }
+
+        return connection;
+    }
 }
