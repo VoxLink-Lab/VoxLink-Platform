1965ab2abb9d50979840857192f5668e57e41e84
YeisraelD <Yeisraeld@gmail.com>
2026-05-15 11:39:07 +0300

    add user model with setters and getters,

--- src/voxlink/server/src/main/model/User.java
+++ src/voxlink/server/src/main/model/User.java
@@ -1,5 +1,84 @@
 package voxlink.server.src.main.model;
 
+import java.sql.Timestamp;
+
 public class User {
-    // TODO
+    private int userId;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String profilePicture;
+    private String status; // online, ofline , away
+    private Timestamp createdAt;
+
+    public User() {
+    }
+
+    public User(int userId, String username, String email, String passwordHash, String profilePicture, String status,
+            Timestamp createdAt) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.profilePicture = profilePicture;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
