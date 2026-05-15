02b58a7309e600ad0a9914001c57b9c189906e5c
YeisraelD <Yeisraeld@gmail.com>
2026-05-15 11:12:15 +0300

    import constants from serverconfig to dbconnector

--- src/voxlink/server/src/main/database/DBConnection.java
+++ src/voxlink/server/src/main/database/DBConnection.java
@@ -3,16 +3,15 @@ package voxlink.server.src.main.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import voxlink.server.src.main.config.ServerConfig;
 
 public class DBConnection {
-    private static final String url = "jdbc:mysql://localhost:3306/voxlink_db";
-    private static final String password = "";
-    private static final String user = "root";
 
     public static Connection connect() {
         Connection connection;
         try {
-            connection = DriverManager.getConnection(DBConnection.url, DBConnection.user, DBConnection.password);
+            connection = DriverManager.getConnection(ServerConfig.DB_URL, ServerConfig.DB_USER,
+                    ServerConfig.DB_PASSWORD);
 
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database!!");
