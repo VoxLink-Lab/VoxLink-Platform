b5db367dec1d548fcbf52d54e3a9033aad94424f
YeisraelD <Yeisraeld@gmail.com>
2026-05-15 12:06:29 +0300

    add workspace model with setters and getters,

--- src/voxlink/server/src/main/model/Workspace.java
+++ src/voxlink/server/src/main/model/Workspace.java
@@ -1,5 +1,74 @@
 package voxlink.server.src.main.model;
 
+import java.sql.Timestamp;
+
 public class Workspace {
-    // TODO
+    private int workspaceId;
+    private String name;
+    private String description;
+    private int ownerId;
+    private String inviteCode;
+    private Timestamp createdAt;
+
+    public Workspace() {
+    }
+
+    public Workspace(int workspaceId, String name, String description, int ownerId, String inviteCode,
+            Timestamp createdAt) {
+        this.workspaceId = workspaceId;
+        this.name = name;
+        this.description = description;
+        this.ownerId = ownerId;
+        this.inviteCode = inviteCode;
+        this.createdAt = createdAt;
+    }
+
+    public int getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(int workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
