2a7cc39b335c78aaf7eb04846a0c988cef1da427
YeisraelD <Yeisraeld@gmail.com>
2026-05-18 08:09:37 +0300

    Restore ServerMain

--- src/voxlink/server/src/main/ServerMain.java
+++ src/voxlink/server/src/main/ServerMain.java
@@ -1,5 +1,16 @@
 package voxlink.server.src.main;
 
+import voxlink.server.src.main.config.ServerConfig;
+import voxlink.server.src.main.network.ServerSocketManager;
+
 public class ServerMain {
-    // TODO
+
+    public static void main(String[] args) {
+        System.out.println("Starting " + ServerConfig.APP_NAME + " v" + ServerConfig.VERSION + "...");
+        
+        // Delegate server start-up to the dedicated manager
+        ServerSocketManager serverManager = new ServerSocketManager(ServerConfig.PORT);
+        serverManager.start();
+    }
 }
+
