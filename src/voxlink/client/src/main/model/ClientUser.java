b20d02394780ff2d2de00666b69b26075e921dfc
YeisraelD <Yeisraeld@gmail.com>
2026-05-31 07:09:50 +0300

    slapped together the client user javafx model

--- src/voxlink/client/src/main/model/ClientUser.java
+++ src/voxlink/client/src/main/model/ClientUser.java
@@ -1,5 +1,32 @@
 package voxlink.client.src.main.model;
 
+import javafx.beans.property.*;
+import voxlink.shared.dto.UserDTO;
+import voxlink.shared.dto.UserStatus;
+
 public class ClientUser {
-    // TODO
+    private final IntegerProperty id = new SimpleIntegerProperty();
+    private final StringProperty username = new SimpleStringProperty();
+    private final StringProperty displayName = new SimpleStringProperty();
+    private final ObjectProperty<UserStatus> status = new SimpleObjectProperty<>();
+
+    public ClientUser(UserDTO dto) {
+        this.id.set(dto.getId());
+        this.username.set(dto.getUsername());
+        this.displayName.set(dto.getDisplayName());
+        this.status.set(dto.getStatus());
+    }
+
+    public int getId() { return id.get(); }
+    public IntegerProperty idProperty() { return id; }
+
+    public String getUsername() { return username.get(); }
+    public StringProperty usernameProperty() { return username; }
+
+    public String getDisplayName() { return displayName.get(); }
+    public StringProperty displayNameProperty() { return displayName; }
+
+    public UserStatus getStatus() { return status.get(); }
+    public ObjectProperty<UserStatus> statusProperty() { return status; }
+    public void setStatus(UserStatus status) { this.status.set(status); }
 }
