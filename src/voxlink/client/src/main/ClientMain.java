c5ccfc0ec0c4f70914a7fc9f8d32a831394047ec
YeisraelD <Yeisraeld@gmail.com>
2026-05-31 03:49:50 +0300

    bootstrapped the javafx main app loop fr

--- src/voxlink/client/src/main/ClientMain.java
+++ src/voxlink/client/src/main/ClientMain.java
@@ -1,5 +1,20 @@
 package voxlink.client.src.main;
 
-public class ClientMain {
-    // TODO
+import javafx.application.Application;
+import javafx.stage.Stage;
+import voxlink.client.src.main.ui.MainWindow;
+
+public class ClientMain extends Application {
+
+    private MainWindow mainWindow;
+
+    @Override
+    public void start(Stage primaryStage) {
+        mainWindow = new MainWindow(primaryStage);
+        mainWindow.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
