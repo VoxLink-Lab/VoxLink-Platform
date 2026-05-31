75a39579834e754ce23ed09a978874e9be6d61d8
YeisraelD <Yeisraeld@gmail.com>
2026-05-31 04:19:50 +0300

    scaffolded the main window bounds no comments purely functional

--- src/voxlink/client/src/main/ui/MainWindow.java
+++ src/voxlink/client/src/main/ui/MainWindow.java
@@ -1,5 +1,27 @@
 package voxlink.client.src.main.ui;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class MainWindow {
-    // TODO
+
+    private final Stage stage;
+    private final BorderPane root;
+
+    public MainWindow(Stage stage) {
+        this.stage = stage;
+        this.root = new BorderPane();
+
+        Scene scene = new Scene(root, 1200, 800);
+
+        this.stage.setTitle("VoxLink");
+        this.stage.setScene(scene);
+        this.stage.setMinWidth(800);
+        this.stage.setMinHeight(600);
+    }
+
+    public void show() {
+        stage.show();
+    }
 }
