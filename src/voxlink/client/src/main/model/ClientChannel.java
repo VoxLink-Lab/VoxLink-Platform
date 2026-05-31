b82ae15a2fa38b10224fd3edd3fba058431c8a0a
YeisraelD <Yeisraeld@gmail.com>
2026-05-31 07:39:50 +0300

    dropped the channel javafx model without comments ez

--- src/voxlink/client/src/main/model/ClientChannel.java
+++ src/voxlink/client/src/main/model/ClientChannel.java
@@ -1,5 +1,36 @@
 package voxlink.client.src.main.model;
 
+import javafx.beans.property.*;
+import voxlink.shared.dto.ChannelDTO;
+
 public class ClientChannel {
-    // TODO
+    private final IntegerProperty id = new SimpleIntegerProperty();
+    private final StringProperty name = new SimpleStringProperty();
+    private final StringProperty description = new SimpleStringProperty();
+    private final BooleanProperty isPrivate = new SimpleBooleanProperty();
+    private final IntegerProperty unreadCount = new SimpleIntegerProperty();
+
+    public ClientChannel(ChannelDTO dto) {
+        this.id.set(dto.getId());
+        this.name.set(dto.getName());
+        this.description.set(dto.getDescription());
+        this.isPrivate.set(dto.isPrivate());
+        this.unreadCount.set(dto.getUnreadCount());
+    }
+
+    public int getId() { return id.get(); }
+    public IntegerProperty idProperty() { return id; }
+
+    public String getName() { return name.get(); }
+    public StringProperty nameProperty() { return name; }
+
+    public String getDescription() { return description.get(); }
+    public StringProperty descriptionProperty() { return description; }
+
+    public boolean getIsPrivate() { return isPrivate.get(); }
+    public BooleanProperty isPrivateProperty() { return isPrivate; }
+
+    public int getUnreadCount() { return unreadCount.get(); }
+    public IntegerProperty unreadCountProperty() { return unreadCount; }
+    public void setUnreadCount(int unreadCount) { this.unreadCount.set(unreadCount); }
 }
